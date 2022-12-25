4752fe152223d683d54b85431155b9d1f2aee44f
msaini0r <mohit.12012540@lpu.in>
2022-12-26 02:57:15 +0530

    Banner Component Completed.

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,24 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>Sale</h1>
+          <p>
+            Buy latest electronic gadgets at 20% discount during Christmas
+            holiday sale only at our website. The sale start's on 25 Dec.
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img className="banner-img" src={BannerImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
