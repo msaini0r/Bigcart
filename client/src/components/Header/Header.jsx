496cfb087110bca158a035a6d43e3f9cbf2ac5f6
msaini0r <mohit.12012540@lpu.in>
2022-12-28 17:35:28 +0530

    backend completed.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
-
-import Search from "./Search/Search";
-import Cart from "../Cart/Cart";
-import AppContext from "../../utils/context";
-
 import "./Header.scss";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
-  const [showCart, setShowCart] = useState(false);
-  const [showSearch, setShowSearch] = useState(false);
+  const [searchModal, setSearchModal] = useState(false);
+  const navigate = useNavigate();
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -25,30 +23,34 @@ const Header = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-  });
+  }, []);
+
+  const { cartCount, showCart, setShowCart } = useContext(Context);
 
   return (
     <>
       <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
         <div className="header-content">
           <ul className="left">
-            <li>Home</li>
-            <li>About</li>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li onClick={() => navigate("/about")}>About</li>
             <li>Categories</li>
           </ul>
-          <div className="center">BIGCART</div>
+          <div className="center" onClick={() => navigate("/")}>
+            JSDEVSTORE.
+          </div>
           <div className="right">
-            <TbSearch onClick={() => setShowSearch(true)} />
+            <TbSearch onClick={() => setSearchModal(true)} />
             <AiOutlineHeart />
             <span className="cart-icon" onClick={() => setShowCart(true)}>
               <CgShoppingCart />
-              <span>5</span>
+              {!!cartCount && <span>{cartCount}</span>}
             </span>
           </div>
         </div>
       </header>
-      {showCart && <Cart setShowCart={setShowCart} />}
-      {showSearch && <Search setShowSearch={setShowSearch} />}
+      {searchModal && <Search setSearchModal={setSearchModal} />}
+      {showCart && <Cart />}
     </>
   );
 };
